1953f0f9ecc11eacc8a874468e4fbc8324beb03b
David Coss <david@davecoss.com>
2013-11-18 07:34:23 -0700

    Added file.

---
+++ src/com/davecoss/Dropbox/APIKeyStore.java
@@ -0,0 +1,21 @@
+package com.davecoss.Dropbox;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class APIKeyStore {
+
+	public String APP_KEY = null;
+	public String APP_SECRET = null;
+	
+	APIKeyStore(String keyfile) throws IOException {
+		Properties configfile = new Properties();
+		InputStream keystream = new FileInputStream(keyfile);
+		configfile.load(keystream);
+		
+		APP_KEY = configfile.getProperty("key");
+		APP_SECRET = configfile.getProperty("secret");
+	}
+}
